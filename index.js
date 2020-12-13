96164e591d2cecb16e925aa2e3fd3effb9abb74c
MINEMin05 <rr05korea@gmail.com>
2020-12-13 11:02:52 +0900

    ang

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const token = process.argv.length == 2 ? process.env.token : "";
-const welcomeChannelName = "안녕하세요";
-const byeChannelName = "안녕히가세요";
-const welcomeChannelComment = "어서오세요.";
-const byeChannelComment = "안녕히가세요.";
+//-------------------------------------------------------------------------
+// Load discord.js and config
+//-------------------------------------------------------------------------
 
+// Load discord.js
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+// Load config
+const config = require('./database/config.json');
+
+//-------------------------------------------------------------------------
+// Connect to lavalink server
+//-------------------------------------------------------------------------
+
+// Load music packages
 const {Manager} = require('@lavacord/discord.js');
 const nodes = [{
     id: '1',
@@ -283,170 +291,12 @@ bot.on('message', async msg => {
     command[cmd](msg, args);
 });
 
-client.on('ready', () => {
-  console.log('켰다.');
-  client.user.setPresence({ game: { name: '오늘도 일' }, status: 'online' })
-});
+//-------------------------------------------------------------------------
+// Connection to bot
+//-------------------------------------------------------------------------
 
-client.on("guildMemberAdd", (member) => {
-  const guild = member.guild;
-  const newUser = member.user;
-  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
+bot.login(config.token);
 
-  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
-
-  member.addRole(guild.roles.find(role => role.name == "GUEST"));
-});
-
-client.on("guildMemberRemove", (member) => {
-  const guild = member.guild;
-  const deleteUser = member.user;
-  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
-
-  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
-});
-
-client.on('message', (message) => {
-  if(message.author.bot) return;
-
-  if(message.content == 'ping') {
-    return message.reply('pong');
-  }
-  if(message.content == 'MINE준') {
-    return message.reply('MINE준 바보 입니다');
-  }
-  const newLocal = '!전체공지';
-  if(message.content == 'Ang') {
-    return message.reply('앙');
-  }
-
-  else if(message.content == '!초대코드2') {
-    client.guilds.array().forEach(x => {
-      x.channels.find(x => x.type == 'text').createInvite({maxAge: 0}) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
-        .then(invite => {
-          message.channel.send(invite.url)
-        })
-        .catch((err) => {
-          if(err.code == 50013) {
-            message.channel.send('**'+x.channels.find(x => x.type == 'text').guild.name+'** 채널 권한이 없어 초대코드 발행 실패')
-          }
-        })
-    });
-  } else if(message.content == '!초대코드') {
-    if(message.channel.type == 'dm') {
-      return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
-    }
-    message.guild.channels.get(message.channel.id).createInvite({maxAge: 0}) // maxAge: 0은 무한이라는 의미, maxAge부분을 지우면 24시간으로 설정됨
-      .then(invite => {
-        message.channel.send(invite.url)
-      })
-      .catch((err) => {
-        if(err.code == 50013) {
-          message.channel.send('**'+message.guild.channels.get(message.channel.id).guild.name+'** 채널 권한이 없어 초대코드 발행 실패')
-        }
-      })
-  } else if(message.content.startsWith('!전체공지2')) {
-    if(checkPermission(message)) return
-    if(message.member != null) { // 채널에서 공지 쓸 때
-      let contents = message.content.slice('!전체공지2'.length);
-      let embed = new Discord.RichEmbed()
-        .setAuthor('공지 MINEMin05 BOT')
-        .setColor('#186de6')
-        .setFooter(`공지 입니다`)
-        .setTimestamp()
-  
-      embed.addField('공지: ', contents);
-  
-      message.member.guild.members.array().forEach(x => {
-        if(x.user.bot) return;
-        x.user.send(embed)
-      });
-  
-      return message.reply('공지를 전송했습니다.');
-    } else {
-      return message.reply('채널에서 실행해주세요.');
-    }
-  } else if(message.content.startsWith(newLocal)) {
-    if(checkPermission(message)) return
-    if(message.member != null) { // 채널에서 공지 쓸 때
-      let contents = message.content.slice('!전체공지'.length);
-      message.member.guild.members.array().forEach(x => {
-        if(x.user.bot) return;
-        x.user.send(`<@${message.author.id}> ${contents}`);
-      });
-  
-      return message.reply('공지를 전송했습니다.');
-    } else {
-      return message.reply('채널에서 실행해주세요.');
-    }
-  } else if(message.content.startsWith('!청소')) {
-    if(message.channel.type == 'dm') {
-      return message.reply('dm에서 사용할 수 없는 명령어 입니다.');
-    }
-    
-    if(message.channel.type != 'dm' && checkPermission(message)) return
-
-    var clearLine = message.content.slice('!청소 '.length);
-    var isNum = !isNaN(clearLine)
-
-    if(isNum && (clearLine <= 0 || 100 < clearLine)) {
-      message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
-      return;
-    } else if(!isNum) {
-      if(message.content.split('<@').length == 2) {
-        if(isNaN(message.content.split(' ')[2])) return;
-
-        var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
-        var count = parseInt(message.content.split(' ')[2])+1;
-        let _cnt = 0;
-
-        message.channel.fetchMessages().then(collected => {
-          collected.every(msg => {
-            if(msg.author.id == user) {
-              msg.delete();
-              ++_cnt;
-            }
-            return !(_cnt == count);
-          });
-        });
-      }
-    } else {
-      message.channel.bulkDelete(parseInt(clearLine)+1)
-        .then(() => {
-          AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "개의 메시지를 삭제했습니다. (이 메세지는 잠시 후에 사라집니다.)");
-        })
-        .catch(console.error)
-    }
-  }
-});
-
-function checkPermission(message) {
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
-    message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function changeCommandStringLength(str, limitLen = 8) {
-  let tmp = str;
-  limitLen -= tmp.length;
-
-  for(let i=0;i<limitLen;i++) {
-      tmp += ' ';
-  }
-
-  return tmp;
-}
-
-async function AutoMsgDelete(message, str, delay = 3000) {
-  let msg = await message.channel.send(str);
-
-  setTimeout(() => {
-    msg.delete();
-  }, delay);
-}
-
-
-client.login(token);
+//-------------------------------------------------------------------------
+// Code by G2G3 - Subscribe me on youtube!
+//-------------------------------------------------------------------------
